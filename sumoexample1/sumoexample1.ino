bb9f411bd8955f81bc8f3f3ab7828fd70b80ead8
Geoff <geoff@gcb5083-114-208.rh.psu.edu>
2016-01-31 10:50:27 -0500

    Sample uploaded with specific formatting

---
+++ sumoexample1/sumoexample1.ino
@@ -0,0 +1,10 @@
+#include <NewPing.h>      //import the NewPing library by Tim Eckel 
+
+void setup() {
+                          //denote the sensor pins as inputs
+                          //denote motor control pins as outputs
+}
+
+void loop() {
+
+}
